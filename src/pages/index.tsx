0e46f4f450e7969aacaa343590432dbb530d24f2
c1334690 <joao.ribeiro@capgemini.com>
2023-10-06 10:09:47 -0300

    feat(is04-header): implementa header da aplicação

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,8 @@
-import { styled } from '../styles'
-
-const Button = styled('button', { 
-  backgroundColor: '$green300',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
-  width: 200,
-
-  '&:hover': {
-    backgroundColor: '$green500',
-  },
-})
 
 export default function Home() {
   return (
     <>
       <h1>Ignite Shop</h1>
-      <Button>Search</Button>
     </>
   )
 }
