bbced99f6f78c7f5013e6aa14e18eda8768e5d8f
c1334690 <joao.ribeiro@capgemini.com>
2023-10-06 00:01:12 -0300

    feat(is01-teste-nav): implementa navbar para navegação de páginas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,9 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <h1>Ignite Shop</h1>
+    <>
+      <h1>Ignite Shop</h1>
+      <nav>Search</nav>
+    </>
   )
 }
