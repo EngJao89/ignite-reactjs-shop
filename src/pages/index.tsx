7fb536f9c39d1d6bc348297b61d24b26db7a95ce
c1334690 <joao.ribeiro@capgemini.com>
2023-10-06 23:41:23 -0300

    feat(is05-home): implementa home com catálogo de produtos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,38 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+import { HomeContainer, Product } from '@/styles/pages/home'
+
+import camiseta1 from '../assets/products/1.png'
+import camiseta2 from '../assets/products/2.png'
 
 export default function Home() {
   return (
     <>
-      <h1>Ignite Shop</h1>
+      <Head>
+        <title>Home | Ignite Shop</title>
+      </Head>
+
+      <HomeContainer>
+        <Product>
+          <Image src={camiseta1} width={520} height={480} alt=""/>
+
+          <footer>
+            <strong>Camiseta X</strong>
+            <span> R$ 79,90</span>
+          </footer>
+        </Product>
+
+        <Product>
+          <Image src={camiseta2} width={520} height={480} alt=""/>
+
+          <footer>
+            <strong>Camiseta Y</strong>
+            <span> R$ 79,90</span>
+          </footer>
+        </Product>
+
+      </HomeContainer>
     </>
   )
 }
